7f83c4af5d7f37695f4b06c554ff6685b9671458
kkumber <angelbertreyes28@gmail.com>
2025-01-10 16:09:44 +0800

    save

--- relight/library/urls.py
+++ relight/library/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('books/', views.BookListView.as_view()),
-    path('books/detail/<slug:slug>/', views.BookDetailView.as_view()),
-    path('books/details/<slug:slug>/comments', views.UserCommentOnBookView.as_view()),
+    path('books/details/<slug:slug>/', views.BookDetailView.as_view()),
+    path('books/details/<slug:slug>/comments/', views.UserCommentOnBookView.as_view()),
 ]
+ 
