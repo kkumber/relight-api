34c3fdedcef921997d55e53c0a745422843af791
kkumber <angelbertreyes28@gmail.com>
2025-01-22 16:28:33 +0800

    init

--- relight/relight/settings.py
+++ relight/relight/settings.py
@@ -86,6 +86,7 @@ CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
 
 # Allow frontend domain for CORS
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Your frontend's address
 ]
