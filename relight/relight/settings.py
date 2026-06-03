e2fe880317c6a3fd06f1180d0b5a6d321f2667d7
Angel Bert Reyes <areyes@insightqgroup.com>
2026-06-03 11:57:18 +0800

    fix: cors

--- relight/relight/settings.py
+++ relight/relight/settings.py
@@ -73,7 +73,6 @@ CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
 
 # Allow frontend domain for CORS
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     os.getenv('FRONTEND_URL'),
 ]
@@ -99,6 +98,7 @@ MEDIA_URL = '/media/'
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -106,7 +106,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'relight.urls'
